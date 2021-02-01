c69fbc5480923392d4a6df1701b12fedfc6ce294
ozansulukpinar <52232302+ozansulukpinar@users.noreply.github.com>
2021-02-02 00:22:39 +0300

    Update Solution0000.cs

--- Coderbyte/Solution0000.cs
+++ Coderbyte/Solution0000.cs
@@ -1,6 +1,9 @@
 /*
 Find Intersection
-Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false.
+Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements: 
+the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will 
+represent a second list of comma-separated numbers (also sorted). Your goal is to return a comma-separated string containing 
+the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false.
 Examples
 Input: new string[] {"1, 3, 4, 7, 13", "1, 2, 4, 13, 15"}
 Output: 1,4,13
